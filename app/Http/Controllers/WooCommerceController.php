cb7173ef69b082f4b3be67d265b59107f5b2bdb6
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-11 16:57:50 +0100

    Dsds

--- app/Http/Controllers/WooCommerceController.php
+++ app/Http/Controllers/WooCommerceController.php
@@ -13,7 +13,8 @@ class WooCommerceController extends Controller
    public function handle(Request $request)
 {
     
-
+ return response()->json(['status'=>'ok']);
+ 
     $order = $request->all();
 
     if (!isset($order['line_items'])) {
