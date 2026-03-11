fd45df8af64bb97409eb0242c6f37501f40b9b84
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-11 17:21:04 +0100

    dsd

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WooCommerceController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return '$request->user();';
+// });
 
 
 Route::post('/wc/webhook', [WooCommerceController::class, 'handle']);
