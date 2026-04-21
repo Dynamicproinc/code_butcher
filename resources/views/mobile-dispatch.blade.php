c8195789658b6f9316637b015ab8929bfd322c23
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-21 16:02:46 +0200

    dsds

--- resources/views/mobile-dispatch.blade.php
+++ resources/views/mobile-dispatch.blade.php
@@ -26,8 +26,9 @@
     .item-details{
       max-height: 220px;
       overflow-y: auto;
-     
+     border-radius: 8px;
       box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+      border:1px solid #eee;
 
     }
    </style>
