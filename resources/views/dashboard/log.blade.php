ca8eb3dca42b86d557f46d38e968b08a070ddaea
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-22 17:59:55 +0200

    fdsfs

--- resources/views/dashboard/log.blade.php
+++ resources/views/dashboard/log.blade.php
@@ -1,41 +1,47 @@
 @extends('mobile')
 @section('title', 'Logs')
 @section('content-mobile')
-    <div>
-        
+    <div class="container">
+        <div style="height: 100vh" class="p-3">
 
-        <div class="">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">{{ __('Date') }}</th>
-                        <th scope="col">{{ __('User') }}</th>
-                        <th scope="col">{{ __('Type') }}</th>
-                        <th scope="col">{{ __('Product ID') }}</th>
-                        <th scope="col">{{ __('Variation ID') }}</th>
-                        <th scope="col">{{ __('WC Stock') }}</th>
-
-                        <th scope="col">{{ __('Quantity') }}</th>
-                        <th scope="col">{{ __('WC Balance') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($logs as $item)
-                        <tr class="@if ($item->type == 'OUT') tbl-red @else tbl-green @endif">
-                            <td>{{ $item->created_at }}</td>
-                            <td>{{ $item->user->name }}</td>
-                            <td>{{ $item->type }}</td>
-                            <td>{{ $item->getProduct()->product_name }}</td>
-                            <td>{{ $item->variation_id }} | {{ $item->variation_code }}</td>
-                            <td>{{ $item->wc_stock }}</td>
-                            <td>{{ $item->quantity }}</td>
-                            <td>{{ $item->balance }}</td>
-
+            <h5><a href="/" style="text-decoration: none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+                    </svg>
+                </a>Logs</h5>
+            <div class="table-responsive">
+                <table class="table table-nowrap">
+                    <thead>
+                        <tr>
+                            <th>{{ __('Date') }}</th>
+                            <th>{{ __('User') }}</th>
+                            <th>{{ __('Type') }}</th>
+                            <th>{{ __('Product ID') }}</th>
+                            <th>{{ __('Variation ID') }}</th>
+                            <th>{{ __('WC Stock') }}</th>
+                            <th>{{ __('Quantity') }}</th>
+                            <th>{{ __('WC Balance') }}</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
+                        @foreach ($logs as $item)
+                            <tr class="@if ($item->type == 'OUT') tbl-red @else tbl-green @endif">
+                                <td>{{ $item->created_at }}</td>
+                                <td>{{ $item->user->name }}</td>
+                                <td>{{ $item->type }}</td>
+                                <td>{{ $item->getProduct()->product_name }}</td>
+                                <td>{{ $item->variation_id }} | {{ $item->variation_code }}</td>
+                                <td>{{ $item->wc_stock }}</td>
+                                <td>{{ $item->quantity }}</td>
+                                <td>{{ $item->balance }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
 
-                </tbody>
-            </table>
             <div>
                 {{ $logs->links() }}
             </div>
