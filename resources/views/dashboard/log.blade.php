ea87d4a7c8790fe306bc0799f94998b9f0e12561
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-23 08:11:51 +0200

    dsdS

--- resources/views/dashboard/log.blade.php
+++ resources/views/dashboard/log.blade.php
@@ -46,6 +46,21 @@
             <div>
                 {{ $logs->links() }}
             </div>
+
+            <video id="video" width="300"></video>
+
+<script src="https://unpkg.com/@zxing/library@latest"></script>
+<script>
+  const codeReader = new ZXing.BrowserBarcodeReader();
+  const videoElement = document.getElementById('video');
+
+  codeReader.decodeFromVideoDevice(null, videoElement, (result, err) => {
+    if (result) {
+      console.log("Barcode:", result.text);
+      alert(result.text);
+    }
+  });
+</script>
         </div>
     </div>
 @endsection
